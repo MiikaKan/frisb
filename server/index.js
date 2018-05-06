b8e054966a55ffe02bcd33b7c022927e309c3d06
Miika K <miika.kanerva@aalto.fi>
2018-05-06 12:19:57 +0300

    gitignore and stuff

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const postaus = new Post({
   players: ["Miika", "Oski"]
 })
 
-app.get('/', (req, res) => res.send(postaus));
+app.get('/', (req, res) => res.send("Hello World!"));
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
