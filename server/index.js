98526ecca5a28815f50947abdcffadfc3f5a62d0
Miika Kanerva <miikakanerva@mac>
2018-05-07 21:26:24 +0300

    back serves index from build

--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Post = require('./models/Post');
 const keys = require('./config/keys');
+const path = require('path');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello World!"));
+app.use(express.static(path.join(__dirname, '../frisb-front/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frisb-front/build', 'index.html'));
+});
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
